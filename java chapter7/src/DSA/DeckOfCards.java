63ae6462a2796ab8541b9dbb951b82683d69ddea
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-02 01:27:19 +0100

    Add files via upload

---
+++ java chapter7/src/DSA/DeckOfCards.java
@@ -0,0 +1,21 @@
+package DSA;
+
+import java.security.SecureRandom;
+
+public class DeckOfCards {
+    private static final SecureRandom randomNumbers = new SecureRandom();
+    private static final int NUMBER_OF_CARDS = 52;
+    private Card[] deck = new Card[NUMBER_OF_CARDS];
+    private int currentCard = 0;
+
+    public DeckOfCards() {
+        String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        for (int count = 0; count < deck.length; count++){
+            deck[count] = new Card(faces[count % 13], suits[count / 13]);
+        }
+    }
+    
+
+}
---
+++ java chapter7/src/DSA/SumArray.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int[] array = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+        int total = 0;
+        for (int counter = 0; counter < array.length; counter++){
+            total += array[counter];
+        }
+        System.out.printf("Total of array element: %d%n", total);
+    }
+}
